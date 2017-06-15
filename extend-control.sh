ad026c0a1ac18015ed9ee915910f92e995df4c32
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2017-06-16 02:22:44 +0900

    Fix error

--- extend-control.sh
+++ extend-control.sh
@@ -4,7 +4,7 @@ source_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${source_path}/scripts/helpers.sh"
 source "${source_path}/scripts/extend_control.sh"
 
-readonly extend_control="$(get_tmux_option "@extend_control", "g")"
+readonly extend_control="$(get_tmux_option "@extend_control" "g")"
 
 tmux bind-key "$extend_control" split-window -l 10 "operation"
 
